7fe47194684a4f48bb9c81f699035559de5e2ee8
Jeff Moorhead <jeff.moorhead1@gmail.com>
2020-07-14 20:58:51 -0400

    Initial commit - server is working

---
+++ main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+    "encoding/json"
+    "io/ioutil"
+    "time"
+    "math/rand"
+    "os"
+)
+
+var quotes quoteHandler = make([]topAFIQuote, 100)
+
+type topAFIQuote struct {
+    Rank int
+    Quote string
+    Movie string
+    Year int
+}
+
+type quoteHandler []topAFIQuote
+
+func getRandomRank(max int) int {
+    return rand.Intn(max)
+}
+
+func checkError(e error) {
+    if e != nil {
+        panic(e)
+    }
+}
+
+func cacheQuotes(filepath string) {
+    data, err := ioutil.ReadFile(filepath)
+    checkError(err)
+    json.Unmarshal(data, &quotes)
+}
+
+func (qh *quoteHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+    rank := getRandomRank(len(*qh))
+    data := (*qh)[rank]
+    fmt.Fprintf(w, "#%d: %s - %s (%d)", data.Rank, data.Quote, data.Movie, data.Year)
+}
+
+func main() {
+    rand.Seed(time.Now().UnixNano())
+
+    quotespath := os.Getenv("GOAFI")
+    cacheQuotes(quotespath)
+    http.Handle("/randomquote", &quotes)
+    http.ListenAndServe(":8000", nil)
+}
